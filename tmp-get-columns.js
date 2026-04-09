b6323e4544316fc5e10a4bea1669721dc70a6bf3
mentor2024 <mentorofaio@gmail.com>
2026-04-09 13:14:03 -0600

    Revert "chore: deploy ping endpoint to verify v-serverless"
    This reverts commit 53c0c074379ae874b7fb57bab90c3e99c05b83f7.
    
    # Conflicts:
    #	scratch.js

--- tmp-get-columns.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { sbQuery } = require('./lib/supabase');
-
-async function run() {
-  const q = `method=POST&table=...` // Wait, sbQuery abstracts Postgrest REST API. We can't query information_schema easily on REST.
-}
