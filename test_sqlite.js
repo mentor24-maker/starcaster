96c105418cf02d3379bf40258fe71d0667e9dd85
mentor2024 <mentorofaio@gmail.com>
2026-04-09 13:14:03 -0600

    Revert "chore: deploy ping endpoint to verify v-serverless"
    This reverts commit 29b92209bf8c94364e047299d8a235cee29d8567.
    
    # Conflicts:
    #	scratch.js

--- test_sqlite.js
+++
@@ -1,5 +0,0 @@
-const db = require('better-sqlite3')('./db.sqlite');
-const rows = db.prepare("SELECT key, value FROM api_settings WHERE key = 'supabase_url' OR key = 'supabase_service_key'").all();
-const env = {};
-rows.forEach(r => { env[r.key] = r.value; });
-console.log("SUPA CONFIG:", env);
