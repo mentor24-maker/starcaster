0fea8ba9fa646668ef117c6c47b5721f479a745b
mentor2024 <mentorofaio@gmail.com>
2026-04-09 13:23:13 -0600

    fix(build): Restore core dependencies required by acquire and observe modules to resolve Vercel crashes

--- query_segments.js
+++
@@ -1,6 +0,0 @@
-const { sbQuery } = require('./lib/supabase');
-async function run() {
-  const res = await sbQuery({ table: 'segments', query: 'select=*&order=created_at.desc' });
-  console.log(JSON.stringify(res.data, null, 2));
-}
-run();
--- test_segments.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const { sbQuery } = require('./lib/supabase');
-async function run() {
-  const res = await sbQuery({ method: 'GET', table: 'segments', query: 'select=*' });
-  console.log(JSON.stringify(res.data, null, 2));
-}
-run();
